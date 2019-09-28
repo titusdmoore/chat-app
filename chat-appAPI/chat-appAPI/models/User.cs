f0de0aaf1305c3125f2e8ed8f5a8807fc4fcb446
Titus Moore <titusdmoore@gmail.com>
2019-09-28 15:50:06 -0400

    Built out User model

--- chat-appAPI/chat-appAPI/models/User.cs
+++ chat-appAPI/chat-appAPI/models/User.cs
@@ -1,9 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace chat_appAPI.models {
     public class User {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string DisplayName { get; set; }
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
     }
 }
