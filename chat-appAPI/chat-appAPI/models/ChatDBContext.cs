ac70e745734b773bfd2b0078a9af840513232ee6
Titus Moore <titusdmoore@gmail.com>
2019-09-28 18:14:20 -0400

    Set User id to null

---
+++ chat-appAPI/chat-appAPI/models/ChatDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_appAPI.models {
+    public class ChatDBContext : DbContext {
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            // Set User Display Name to unique
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.DisplayName)
+                .IsUnique();
+        }
+
+        public ChatDBContext(DbContextOptions options) : base(options) {
+
+        }
+    }
+}
